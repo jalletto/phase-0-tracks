5e17406f3816eb95e8037dc4f6aef6b3e49862db
Joshua Alletto <joshalletto@gmail.com>
2017-05-09 10:57:36 -0500

    add loop for adoption question and changed variable types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,11 +1,29 @@
 puts "Please input your hamster's name."
 name = gets.chomp
 puts "Enter 1 - 10 for volume level."
-volume = gets.chomp
+volume = gets.chomp.to_i
 puts "Enter fur color."
 color = gets.chomp
-puts "Good for adoption?"
+puts "Good for adoption (y or n)?"
 adoption = gets.chomp
+
+valid_imput = false
+loop do
+  if adoption == "y" || adoption == "yes"
+    adoption = true
+    valid_imput = true
+    break
+  elsif adoption == "n" || adoption == "no"
+    adoption = false
+    valid_imput = true
+    break
+  else
+    puts "Please try again."
+    adoption = gets.chomp
+  end
+end
+
+
 puts "Enter age of hamster."
 age = nil
 age = gets.chomp
