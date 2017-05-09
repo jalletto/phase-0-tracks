f2798ae3b5d11f2f52e0f9d52d3f00c19e062b89
Joshua Alletto <joshalletto@gmail.com>
2017-05-09 10:36:59 -0500

    add questions and variables for hamster program

---
+++ ruby/hamsters.rb
@@ -0,0 +1,13 @@
+puts "Please input your hamster's name."
+name = gets.chomp
+puts "Enter 1 - 10 for volume level."
+volume = gets.chomp
+puts "Enter fur color."
+color = gets.chomp
+puts "Good for adoption?"
+adoption = gets.chomp
+puts "Enter age of hamster."
+age = nil
+age = gets.chomp
+
+
