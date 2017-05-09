f08c02e2a71c55fcaa9d4b9685f05edcd3c95070
Joshua Alletto <joshalletto@gmail.com>
2017-05-09 11:17:54 -0500

    add loops for volume

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,20 +2,26 @@ puts "Please input your hamster's name."
 name = gets.chomp
 puts "Enter 1 - 10 for volume level."
 volume = gets.chomp.to_i
+loop do
+  if volume >= 1 && volume <= 10
+
+    break
+  else
+    puts "Enter 1 - 10 for volume level."
+    volume = gets.chomp.to_i
+  end
+end
 puts "Enter fur color."
 color = gets.chomp
 puts "Good for adoption (y or n)?"
 adoption = gets.chomp
 
-valid_imput = false
 loop do
   if adoption == "y" || adoption == "yes"
     adoption = true
-    valid_imput = true
     break
   elsif adoption == "n" || adoption == "no"
     adoption = false
-    valid_imput = true
     break
   else
     puts "Please try again."
@@ -23,9 +29,16 @@ loop do
   end
 end
 
-
 puts "Enter age of hamster."
-age = nil
+
 age = gets.chomp
+if age == ""
+  age = nil
+else
+  age = age.to_i
+end
+
+
+puts "Name: #{name}\nVolume:#{volume}\nFur Color:#{color}\nAdoption:#{adoption}\nAge:#{age}"
 
 
