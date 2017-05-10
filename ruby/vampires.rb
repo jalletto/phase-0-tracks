3178641472aabd6d7de6fd8293561ae43fd75f75
Joshua Alletto <joshalletto@gmail.com>
2017-05-10 13:12:50 -0500

    add interview questions and variables for answers

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+likes_garlic = gets.chomp
+puts "Would you like to enroll in our health insurance"
+insurance = gets.chomp
+
+puts "Name:#{name}\nAge:#{age}\nYOB:#{birth_year}\nLikes garlic:#{likes_garlic}\nInsurance:#{insurance}\n"
