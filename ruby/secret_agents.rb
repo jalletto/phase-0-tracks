8f109f9f151ab79c91ffd01e8260536ef36ff20b
Joshua Alletto <joshalletto@gmail.com>
2017-05-11 16:07:56 -0500

    add method for encrypt and decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,4 +1,24 @@
+def encrypt(word)
+  counter = 0
 
+  while counter < word.length
+    print word[counter].next
+    counter += 1
+  end
+
+end
+
+def decrypt(crypt)
+  counter = 0
+  alphabet = "abcdefghijklmnopqrstuvwz"
+
+  while counter < crypt.length
+    letter = crypt[counter]
+    result = alphabet.index(letter)
+    print alphabet[result - 1]
+    counter += 1
+  end
+end
 
 
 
