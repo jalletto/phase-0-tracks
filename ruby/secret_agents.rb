7c57f2c78597758e69bf2455e60c9c415ac0ce09
Joshua Alletto <joshalletto@gmail.com>
2017-05-12 16:45:38 -0500

    add interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,46 +1,57 @@
 def encrypt(word)
   counter = 0
 
-  while counter < word.length do
+  while counter < word.to_s.length do
     new_letter = word[counter].next
     if new_letter == "aa"
       new_letter = "a"
     end
-    print new_letter.to_s
+    print new_letter
     counter += 1
   end
 
 end
 
 def decrypt(crypt)
-  counter = 0
-  alphabet = "abcdefghijklmnopqrstuvwz"
+  count = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-  while counter < crypt.length do
-    letter = crypt[counter]
+  while count < crypt.to_s.length do
+    letter = crypt[count]
     result = alphabet.index(letter)
     print alphabet[result - 1]
-    counter += 1
+    count += 1
   end
 
 end
 
-# puts encrypt("abc") #should return "bcd"
-# puts encrypt("zed") #should return "afe"
-# puts decrypt("bcd") #should return "abc"
-# puts decrypt("afe") #should return "zed"
+puts encrypt("abc") #should return "bcd"
+puts encrypt("zed") #should return "afe"
+puts decrypt("bcd") #should return "abc"
+puts decrypt("afe") #should return "zed"
 
-# decrypt(encrypt("swordfish")) I actually can't get this to work right at all.
 
-puts "Type D to decrypt a password. Type E to encrypt a password."
-opporation = gets.chomp
+puts decrypt(encrypt("swordfish"))
+puts encrypt(decrypt("txpsegjti"))
 
-if opporation == "D" || opporation == "d"
-  puts "Enter password for decryption."
-  decrypt(gets.chomp)
-elsif opporation == "E" || opporation == "e"
-  puts "Enter password encryption."
-  encrypt(gets.chomp)
+
+valid_input = false
+until valid_input do
+  puts "Type D to decrypt a password. Type E to encrypt a password."
+  opporation = gets.chomp
+
+
+  if opporation == "D" || opporation == "d"
+    puts "Enter password for decryption."
+    decrypt(gets.chomp)
+    valid_input = true
+  elsif opporation == "E" || opporation == "e"
+    puts "Enter password encryption."
+    encrypt(gets.chomp)
+    valid_input = true
+  else
+    valid_input = false
+  end
 end
 
 
