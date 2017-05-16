e3cca455dcdd5ab828f0d76bc49d7355f7cd315f
Joshua Alletto <joshalletto@gmail.com>
2017-05-15 20:34:50 -0500

    add suedo code

---
+++ ruby/hashes.rb
@@ -0,0 +1,7 @@
+# create an empty hash. Ask user for name, age, number of children, decor theme, do you entertain?, what is your favorite type of light bulb.
+# create a symbol for each value and assign it to the hash.
+#print hash
+# ask the user to enter any fields they would like to update.
+# ask the user which field and what to update it to.
+# convert string entered by user to a key
+#ask for updated info and add it to key
