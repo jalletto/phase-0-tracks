f99f751d58649222776c277cda5ac48f75747a6a
Joshua Alletto <joshalletto@gmail.com>
2017-05-16 11:01:25 -0500

    add map and each assignments

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,27 @@ def say_goodbye
 end
 
 say_goodbye { |name1, name2| puts "Come back soon #{name1} and #{name2}!"}
+
+colors = ["green", "blue", "yellow", "red"]
+
+user_data = {
+  name: "josh",
+  age: "32",
+  eye_color:"red",
+  home_town:"Chicago"
+}
+
+colors.each do |color|
+  puts "The color is #{color}"
+end
+
+colors.map! do |color|
+  color.upcase
+end
+
+p colors
+
+user_data.each do |key, value|
+  puts "#{value}"
+end
+
