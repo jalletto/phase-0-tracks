b36a7e6500ce44ad24acba526f16e69beed2bc75
Joshua Alletto <joshalletto@gmail.com>
2017-05-15 20:43:27 -0500

    add a hash for form data and UI for getting data

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,3 +5,19 @@
 # ask the user which field and what to update it to.
 # convert string entered by user to a key
 #ask for updated info and add it to key
+
+
+client_info = {}
+
+puts "Enter name."
+client_info[:name] = gets.chomp
+puts "How old are you?"
+client_info[:age] = gets.chomp.to_i
+puts "How many children do you have?"
+client_info[:number_children] = gets.chomp.to_i
+puts "Do you like to party?"
+party = gets.chomp
+puts "What is your favorite type of light bulb?"
+client_info[:fav_light_bulb] = gets.chomp
+
+p client_info
