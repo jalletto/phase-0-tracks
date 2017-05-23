aa4384f5e55069ea7230630e3df22ce4ecf9a5c2
Joshua Alletto <joshalletto@gmail.com>
2017-05-23 11:08:35 -0500

    add kitten class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,72 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+
+#   def speak(number)
+#     number.times do
+#      p "woof"
+#     end
+#   end
+
+#   def roll_over()
+#     p "*Rolls Over*"
+#   end
+
+#   def age_in_dog_years(human_years)
+#    p human_years * 7
+#   end
+
+#   def wag_tail()
+#     p "*Wags Tail*"
+#   end
+
+#   def initialize()
+#     p "Initializing new puppy instance."
+#   end
+
+# end
+
+# fido = Puppy.new
+# fido.fetch("ball")
+# fido.speak(5)
+# fido.roll_over
+# if fido.age_in_dog_years(15) == 105
+#  p true
+# end
+# fido.wag_tail()
+
+class Kittens
+
+  def meow(i)
+   p "meow " * i
   end
 
 end
+
+def create_kitten(name)
+  p name = Kittens.new
+end
+
+
+
+
+don = Kittens.new
+
+don.meow(5)
+
+# while Kitten instances is less than 50 create a new instance of Kitten starting with Kitten1 and going up.
+
+
+
+kittens_created = 0
+
+while kittens_created < 50
+  p kitten1 = Kittens.new
+  kittens_created += 1
+end
+
+
 
