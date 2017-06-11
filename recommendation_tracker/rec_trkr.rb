169bb484d4f4582d6fd3161d89cfc914b3617e39
Joshua Alletto <joshalletto@gmail.com>
2017-06-10 20:46:30 -0500

    add title and friend view for recs.

--- recommendation_tracker/rec_trkr.rb
+++ recommendation_tracker/rec_trkr.rb
@@ -33,16 +33,17 @@ db.execute(create_books_cmd)
 db.execute(create_friends_cmd)
 
 
-# All methods for BOOKS table
+# All methods for BOOKS table -----------------------------------------------
+
 def add_book(db, title, author_first, author_last, user_message, friend_id)
   db.execute("INSERT INTO books (title, author_first, author_last, user_message, friend_id) VALUES (?, ?, ?, ?, ?)", [title, author_first, author_last, user_message, friend_id])
 end
 
 def view_books(db)
-  books = db.execute('SELECT * FROM books')
+  books = db.execute('SELECT * FROM books JOIN friends ON books.friend_id = friends.id')
 
   books.each do |book|
-    puts "\n#{books.index(book) + 1}. #{book[1]} by #{book[2]} #{book[3]} friend: #{book[5]}"
+    puts "\n#{books.index(book) + 1}. #{book[1]} by #{book[2]} #{book[3]} recommended by #{book[-1]}"
   end
 
 end
@@ -57,73 +58,81 @@ def add_friend(db, friends_name)
 end
 
 def view_friends(db)
-  friends = db.execute('SELECT * FROM friends')
+  friends = db.execute('SELECT friends.name, books.title FROM friends JOIN books ON friends.id = books.friend_id')
 
   friends.each do |friend|
-    puts "\n#{friends.index(friend) + 1}. #{friend[1]}"
+    puts "\n#{friends.index(friend) + 1}. #{friend[0]} recommended #{friend[1]}"
   end
 end
 
-# Methods for both tables
-  def view_all(db)
-    all_books = db.execute('SELECT title, author_first, author_last FROM books INNER JOIN friends ON books.friend_id = friends.id')
+p friends = db.execute('SELECT friends.name, books.title FROM friends JOIN books ON friends.id = books.friend_id')
+
+
+
+
+
+#UI-----------------------------------
+
+
+mode = ''
+until mode == 'q'
+
+  puts "\nA - add book\nD - delete book\nV - view your recommendations\nQ - quit\n"
+
+  mode = gets.chomp.downcase
+
+  if mode == 'a'
+# Adding A Book
+    done_adding_books = false
+
+    until done_adding_books == true
+      puts "Awesome! A new recommendation! Who gave it?"
+      new_friend = gets.chomp
+      old_friends = db.execute('SELECT name FROM friends')
+
+    if !old_friends.flatten.include? new_friend
+
+      add_friend(db, new_friend)
+    end
+
+      friend_id = db.execute('SELECT id FROM friends WHERE name = ?', [new_friend])
+
+
+      puts "\nEnter the book's title."
+      title = gets.chomp
+      puts "\nEnter the author's first name."
+      author_first = gets.chomp
+      puts "\nEnter the author's last name."
+      author_last = gets.chomp
+      puts "\nWhat where you talking about when this book was recommended to you?"
+      user_message = gets.chomp
+      add_book(db, title, author_first, author_last,user_message, friend_id)
+
+      puts "Add another? Y or N"
+
+        add_another = gets.chomp.downcase
+        if add_another == 'n'
+          done_adding_books = true
+        end
+      end
+# Deleting a book
+
+    elsif mode == 'd'
+      view_books(db)
+      puts "\nType the title of the book you want to delete."
+      book_to_delete = gets.chomp
+      delete_book(db, book_to_delete)
+# View your Recommendations
+  elsif mode == 'v'
+    puts "Type F to view your friends of T to view by title."
+    view_type = gets.chomp.downcase
+    if view_type == 'f'
+      view_friends(db)
+    elsif view_type == 't'
+      view_books(db)
+    end
   end
-
-#UI
-# mode = ''
-# until mode == 'q'
-
-#   puts "\nA - add book\nD - delete book\nV - view your recommendations\nQ - quit\n"
-
-#   mode = gets.chomp.downcase
-
-#   if mode == 'a'
-# # Adding A Book
-#     done_adding_books = false
-
-#     until done_adding_books == true
-#       puts "Awesome! A new recommendation! Who gave it?"
-#       new_friend = gets.chomp
-#       old_friends = db.execute('SELECT name FROM friends')
-
-#     if !old_friends.flatten.include? new_friend
-
-#       add_friend(db, new_friend)
-#     end
-
-#       friend_id = db.execute('SELECT id FROM friends WHERE name = ?', [new_friend])
-
-
-#       puts "\nEnter the book's title."
-#       title = gets.chomp
-#       puts "\nEnter the author's first name."
-#       author_first = gets.chomp
-#       puts "\nEnter the author's last name."
-#       author_last = gets.chomp
-#       puts "\nWhat where you talking about when this book was recommended to you?"
-#       user_message = gets.chomp
-#       add_book(db, title, author_first, author_last,user_message, friend_id)
-
-#       puts "Add another? Y or N"
-
-#         add_another = gets.chomp.downcase
-#         if add_another == 'n'
-#           done_adding_books = true
-#         end
-#       end
-# # Deleting a book
-
-#     elsif mode == 'd'
-#       view_books(db)
-#       puts "\nType the title of the book you want to delete."
-#       book_to_delete = gets.chomp
-#       delete_book(db, book_to_delete)
-# # View your Recommendations
-#   elsif mode == 'v'
-
-#   view_books(db)
-#   end
-# end
+end
 
 
 
